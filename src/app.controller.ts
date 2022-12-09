a4c7baa79a3aef56a9f117c8d1d935d8f19953cc
diego-wendt <diego_wendt@hotmail.com>
2022-12-08 22:14:44 -0300

    feat add SignIn

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,40 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  ValidationPipe,
+  HttpStatus,
+  HttpException,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthService } from './core/auth/auth.service';
 import { CreateUserDto } from './twitter/dto/create-user.dto';
+import { CredentialsDTO } from './twitter/dto/credentials.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService,
-    private readonly authService: AuthService) { }
-
+  constructor(
+    private readonly appService: AppService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Post('/auth/signup')
   async signUp(@Body(ValidationPipe) createUserDto: CreateUserDto) {
     await this.authService.signUp(createUserDto);
     return {
-      message: 'Cadastro realizado.'
+      message: 'Cadastro realizado.',
+    };
+  }
+
+  @Post('/auth/signin')
+  async signIn(@Body() credentials: CredentialsDTO) {
+    try {
+      return await this.authService.signIn(credentials);
+    } catch (error) {
+      throw new HttpException(
+        { mensagem: 'Usuário ou senha inválidos' },
+        HttpStatus.BAD_REQUEST,
+      );
     }
   }
 }
--- src/core/auth/auth.service.ts
+++ src/core/auth/auth.service.ts
@@ -1,50 +1,80 @@
-import * as bcrypt from 'bcrypt'
+import * as bcrypt from 'bcrypt';
 import { Inject, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { UserEntity } from 'src/twitter/entities/user.entity';
 import { CreateUserDto } from 'src/twitter/dto/create-user.dto';
 import { CredentialsDTO } from 'src/twitter/dto/credentials.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        @Inject('USER_REPOSITORY')
-        private userRepository: Repository<UserEntity>
-    ) { }
+  constructor(
+    @Inject('USER_REPOSITORY')
+    private userRepository: Repository<UserEntity>,
+    private jwtService: JwtService,
+  ) {}
 
-    signUp(user: CreateUserDto): Promise<UserEntity> {
-        return new Promise(async (resolve, reject) => {
-            try {
-                const { name, email, password } = user;
-                const userToCreate = this.userRepository.create()
-                userToCreate.nome = name
-                userToCreate.email = email
-                userToCreate.ativo = true
-                userToCreate.salt = await bcrypt.genSalt()
-                userToCreate.password = await bcrypt.hash(password, userToCreate.salt)
-                this.userRepository.save(userToCreate)
-                delete userToCreate.password
-                delete userToCreate.salt
-                resolve(userToCreate)
-            } catch (error) {
-                reject(error)
-            }
+  signUp(user: CreateUserDto): Promise<UserEntity> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const { name, email, password } = user;
+        const userToCreate = this.userRepository.create();
+        userToCreate.nome = name;
+        userToCreate.email = email;
+        userToCreate.ativo = true;
+        userToCreate.salt = await bcrypt.genSalt();
+        userToCreate.password = await bcrypt.hash(password, userToCreate.salt);
+        this.userRepository.save(userToCreate);
+        delete userToCreate.password;
+        delete userToCreate.salt;
+        resolve(userToCreate);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
 
-        })
-    }
-
-    async checkCredentials(credentials: CredentialsDTO) {
+  signIn(credentials: CredentialsDTO): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+      try {
         const { email, password } = credentials;
         const user = await this.userRepository.findOne({
-            where: {
-                email: email,
-                ativo: true
-            }
-        })
+          where: { email: email },
+        });
 
-        if (user && (await user.checkPassword(password))) {
-            return user;
+        if (!user?.salt) {
+          console.log('não tem usuario');
+          reject('não tem usuario');
         }
-        return null;
+
+        if (await user.checkPassword(password)) {
+          const jwtPayload = {
+            id: user.id,
+            name: user.nome,
+            email: user.email,
+            // TODO
+            // role: user.role,
+          };
+          resolve(this.jwtService.sign(jwtPayload));
+        }
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  async checkCredentials(credentials: CredentialsDTO) {
+    const { email, password } = credentials;
+    const user = await this.userRepository.findOne({
+      where: {
+        email: email,
+        ativo: true,
+      },
+    });
+
+    if (user && (await user.checkPassword(password))) {
+      return user;
     }
+    return null;
+  }
 }
